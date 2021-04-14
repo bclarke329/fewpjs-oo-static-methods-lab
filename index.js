c83bc9948200de5edeb3e7f197dbc6c41236a75b
bclarke329 <bmendoza329@hotmail.com>
2021-04-13 23:23:34 -0500

    commit tree 21ceedf4dfc7420152ecfaa2dd0756f025788476

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '')
+  }
+
+  static titleize(string) {
+    let excludedWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let arr = []
+    arr = string.split(' ')
+    return arr.map((word, i) => {return excludedWords.includes(word) && i!=0 ? [word] : word.charAt(0).toUpperCase() + word.slice(1); 
+    }).join(' ');
+  }
+  
+
 }
